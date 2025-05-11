8c694c26d8d6d4f1b0518863267f093a9f2cca8b
EduardBeltranLopez <edubeltranlp@gmail.com>
2025-05-11 14:04:09 +0200

    AudioLata

--- Assets/Project/Scripts/SceneManager.cs
+++ Assets/Project/Scripts/SceneManager.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class SceneManager : MonoBehaviour
+public class ChangeScene : MonoBehaviour
 {
 
+    public void ChargeScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
 
-
+    public void Exit()
+    {
+        Application.Quit();
+    }
 
 }
